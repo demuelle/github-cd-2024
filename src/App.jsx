afec389a296394bd5d14bf7ebf5f67c6659c7195
demuelle <dmueller@trilogyed.com>
2024-08-20 13:27:18 -0500

    Changes h1 without test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ViteReactRender...CICD</h1>
+      <h1>ViteReactRenderCI to the sizzle dizzle CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
